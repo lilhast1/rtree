0e9523e5fc1cf9370c10edfed9859feb7b111569
Tarik Hastor <thastor1@etf.unsa.ba>
2025-10-27 13:14:32 +0100

    interfejs

--- src/rtree/rtree.h
+++ src/rtree/rtree.h
@@ -1,11 +1,28 @@
+#include <initializer_list>
 #include <vector>
 
+template <typename T>
 class RTree {
    public:
     RTree();
+
+    template <typename Itr>
+    RTree(Itr, Itr);
+
+    RTree(std::initializer_list<T>);
+    RTree(const RTree<T>&);
+    RTree(RTree<T>&&);
+    RTree<T>& operator=(const RTree<T>&);
+    RTree<T>& operator=(RTree<T>&&);
     ~RTree();
 
-    void insert(int id, const float* minBounds, const float* maxBounds);
-    void remove(int id, const float* minBounds, const float* maxBounds);
+    void swap(RTree<T>&);
+
+    void insert(const T&);
+
+    template <typename Itr>
+    void insert(Itr, Itr);
+
+    void remove(const T&);
     bool search(const float* minBounds, const float* maxBounds, std::vector<int>& results) const;
 };
