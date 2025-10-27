eba40871bf98db5c8d0292eef33159b4a8bfe840
Tarik Hastor <thastor1@etf.unsa.ba>
2025-10-27 16:48:43 +0100

    c++23 interfejs sry

--- src/rtree/rtree.h
+++ src/rtree/rtree.h
@@ -1,28 +1,62 @@
+#include <any>
 #include <initializer_list>
+#include <iterator>
 #include <vector>
 
 template <typename T>
 class RTree {
+   protected:
+    virtual const_iterator queryImpl(const std::function<bool(const T&)>& pred) const = 0;
+
    public:
-    RTree();
+    using iterator = std::any_iterator<T&, std::input_iterator_tag>;
+    using const_iterator = std::any_iterator<const T&, std::input_iterator_tag>;
+
+    RTree() = default;
 
     template <typename Itr>
-    RTree(Itr, Itr);
+    RTree(Itr first, Itr last) {
+        insert(first, last);
+    }
 
-    RTree(std::initializer_list<T>);
-    RTree(const RTree<T>&);
-    RTree(RTree<T>&&);
-    RTree<T>& operator=(const RTree<T>&);
-    RTree<T>& operator=(RTree<T>&&);
-    ~RTree();
+    RTree(std::initializer_list<T> list) { insert(list.begin(), list.end()); }
+    RTree(const RTree<T>&) = default;
+    RTree(RTree<T>&&) = default;
+    RTree<T>& operator=(const RTree<T>&) = default;
+    RTree<T>& operator=(RTree<T>&&) = default;
+    virtual ~RTree() = default;
 
-    void swap(RTree<T>&);
+    virtual void swap(RTree<T>&) = 0;
 
-    void insert(const T&);
+    virtual void insert(const T&) = 0;
 
     template <typename Itr>
-    void insert(Itr, Itr);
+    void insert(Itr first, Itr last) {
+        while (first != last) {
+            this->insert(*first++);
+        }
+    }
 
-    void remove(const T&);
-    bool search(const float* minBounds, const float* maxBounds, std::vector<int>& results) const;
+    virtual void remove(const T&) = 0;
+
+    virtual iterator begin() = 0;
+    virtual iterator end() = 0;
+
+    virtual const_iterator begin() const = 0;
+    virtual const_iterator end() const = 0;
+
+    template <typename Predicate>
+    iterator query(Predicate pred) {
+        return queryImpl(pred);
+    }
+
+    template <typename Predicate>
+    const_iterator query(Predicate pred) const {
+        return queryImpl(pred);
+    }
+
+    virtual unsigned int size() const = 0;
+
+    virtual bool empty() const = 0;
+    virtual void clear() = 0;
 };
